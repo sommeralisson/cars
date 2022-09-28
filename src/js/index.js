7b28ca61d22b8d0ba8ec97e2a3ffc6d17c57829b
sommeralisson <alissonsommer1@gmail.com>
2022-09-27 23:21:30 -0300

    Inserindo validações JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,4 @@
 let sUrlState = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
-  , sUrlCity  = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/SC/municipios'
-
-let teste = []
 
 // async function jPegaDados() {
 //   await fetch(sUrlState)
@@ -19,32 +16,69 @@ let teste = []
 //     })
 // }
 
-// window.addEventListener('load', () => {
-//   setTimeout(function() {
-//     var estado = document.querySelector('#states')
-//     var estadoss = estado.querySelectorAll('option')
+async function jDoHtmlStates() {
+  let bFirst = true
+  const oResponse = await fetch(sUrlState)
+  const oData = await oResponse.json()
 
-//     estadoss.forEach(element => {
-//       element.addEventListener('change', (target) => {
-//         console.log(target)
-//       })
-//     })
-//   }, 500);
+  const oLabelCity = document.querySelector('#city')
+  const oLabelSelect = document.querySelector('#state')
+  const oSelect = document.createElement('select')
+  oSelect.setAttribute('id', 'states')
+        
+  oLabelSelect.append(oSelect)
+        
+  oData.map((state) => {
+    const oOption = document.createElement('option')
+    oOption.setAttribute('value', state.sigla)
+    oOption.innerText = state.nome
+
+    oSelect.appendChild(oOption)
+  })
   
-//   jPegaDados()
-// })
+  oSelectCity = document.createElement('select')
+  oLabelCity.append(oSelectCity)
 
+  if (bFirst) {
+    let sUf = document.querySelector('#states').firstElementChild.getAttribute('value')
 
-// console.log(estadoss)
+    let oResponseCity = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${sUf}/municipios`)
+    let oDataCity = await oResponseCity.json()
 
+    
 
+    oDataCity.map((city) => {
+      const oOptionCity = document.createElement('option')
+      oOptionCity.innerText = city.nome
 
-// $.getJSON(sUrlCity, function(data) {
-//   let sHtmlCity = '<select>'
-//   $.each(data, function(indice, element) {
-//     sHtmlCity += '<option>'+element.nome+'</option>'
-//   })
-//   sHtmlCity += '</select>'
+      oSelectCity.appendChild(oOptionCity)
+    })
 
-//   $("#city").append(sHtmlCity)
-// })
+  }
+  
+  let oSelected = document.querySelector('#states')
+  oSelected.addEventListener('change', (event) => {
+    console.log(event.target.value)
+
+    
+    let testee = jGeraRequisicao('city', event.target.value)
+    console.log(testee.then(response => console.log(response)))
+    
+  });
+}
+
+async function jGeraRequisicao(type, uf) {
+  if (type == 'state') {
+    const oResponseSelect = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+
+    return oResponseSelect.json()
+  } else {
+    let sUf = uf
+    const oResponseSelect = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${sUf}/municipios`)
+    
+    return oResponseSelect.json()
+  }
+
+}
+
+jDoHtmlStates()
